2a05d25c3439cbbaf0dbce9402db9353b1eb7366
phil <phil@samsung-ws>
2020-05-11 09:06:15 -0400

    updates for properties and app home

--- src/main/java/financialanalyzer/application/FinancialAnalyzerApplication.java
+++ src/main/java/financialanalyzer/application/FinancialAnalyzerApplication.java
@@ -5,6 +5,8 @@
  */
 package financialanalyzer.application;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import financialanalyzer.config.AppConfig;
 import java.io.File;
 import javax.annotation.PostConstruct;
@@ -20,6 +22,8 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -34,12 +38,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJms
 public class FinancialAnalyzerApplication implements ApplicationRunner {
-
+    
     private static final Logger logger = LoggerFactory.getLogger(FinancialAnalyzerApplication.class.getName());
-
+    
     @Autowired
     private AppConfig appConfig;
-
+    
+    @Autowired
+    private ResourceLoader resourceLoader;
+    
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(FinancialAnalyzerApplication.class);
         //app.setWebApplicationType(WebApplicationType.NONE);
@@ -47,12 +54,12 @@ public class FinancialAnalyzerApplication implements ApplicationRunner {
         //SpringApplication.run(SearchScraperApplication.class, args);
 
     }
-
+    
     @Override
     public void run(ApplicationArguments args) throws Exception {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @PostConstruct
     private void init() {
         logger.info("Initializing FinancialAnalyzer");
@@ -62,18 +69,30 @@ public class FinancialAnalyzerApplication implements ApplicationRunner {
         this.createDirIfNotExists(appConfig.getCompanyNewsDownloadDir());
         this.createDirIfNotExists(appConfig.getStockHistoryDownloadDir());
         this.createDirIfNotExists(appConfig.getLogDir());
-        
-        System.setProperty("log.path", appConfig.getLogDir());
-        FactoryFriend.reset();
+        //this.reInitLogging();
         logger.info("Finished Initializing FinancialAnalyzer");
     }
-
+    
+    private void reInitLogging() {
+        System.setProperty("log.path", appConfig.getLogDir());
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        context.reset();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(context);
+        Resource logConfigfile = resourceLoader.getResource("classpath:logback-spring.xml");
+        try {
+            configurator.doConfigure(logConfigfile.getFile());
+        } catch (Exception e) {
+            logger.error("Error in initializing logging");
+        }
+    }
+    
     private void createDirIfNotExists(String _fileDir) {
         File dirFile = new File(_fileDir);
-
+        
         if (!dirFile.isDirectory() && !dirFile.isFile()) {
             dirFile.mkdirs();
         }
     }
-
+    
 }

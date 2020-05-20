ddc5bf5b183d3d7abf4f5bd632a0a76090fc5d11
phil <phil@samsung-ws>
2020-05-20 12:18:11 -0400

    pagination and expansion of news apis

--- src/main/java/financialanalyzer/controller/v1/NewsRestController.java
+++ src/main/java/financialanalyzer/controller/v1/NewsRestController.java
@@ -26,19 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/news")
 public class NewsRestController {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(NewsRestController.class.getName());
     @Autowired
     private CompanyRepo companySearchRepo;
     @Autowired
     private CompanyNewsService companyNewsServiceImpl;
-    
+
     @Autowired
     private CompanyNewsRepo companyNewsSearchRepo;
-    
+
     @Autowired
     private CompanyNewsDownloadDriver companyNewsDownloadDriver;
-    
+
     @RequestMapping(value = "/fetchLatestData", method = RequestMethod.POST, produces = "application/json")
     public RestResponse triggerNewsDownload() {
         RestResponse restResponse = new RestResponse();
@@ -48,19 +48,19 @@ public class NewsRestController {
     }
 
     @RequestMapping(value = "/latest/{start}", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse getNewsStartingWith(@PathVariable("start") int _start) {
-        return this.getNewsStartingWithInRange(_start, 25);
+    public RestResponse getLatestNewsStartingWith(@PathVariable("start") int _start) {
+        return this.getLatestNewsStartingWithInRange(_start, 25);
     }
-    
+
     @RequestMapping(value = "/latest/{start}/{numberOfItems}", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse getNewsStartingWithInRange(@PathVariable("start") int _start, @PathVariable("numberOfItems") int _numberOfItems) {
+    public RestResponse getLatestNewsStartingWithInRange(@PathVariable("start") int _start, @PathVariable("numberOfItems") int _numberOfItems) {
         RestResponse restResponse = new RestResponse();
-        CompanySearchProperties csp = new CompanySearchProperties();
+        //CompanySearchProperties csp = new CompanySearchProperties();
         //csp.setCompanyId(_id);
 
-        List<Company> companies = this.companySearchRepo.searchForCompany(csp);
+        //List<Company> companies = this.companySearchRepo.searchForCompany(csp);
         List<CompanyNewsItem> companyNewsItems = new ArrayList<>();
-        
+
         CompanyNewsSearchProperties cnsp = new CompanyNewsSearchProperties();
         //cnsp.setStockExchange(company.getStockExchange());
         //cnsp.setStockSymbol(company.getStockSymbol());
@@ -78,12 +78,12 @@ public class NewsRestController {
         return restResponse;
 
     }
-    
+
     @RequestMapping(value = "/latest", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse getNews() {
-        return this.getNewsStartingWith(0);
+    public RestResponse getLatestNews() {
+        return this.getLatestNewsStartingWith(0);
     }
-    
+
     @RequestMapping(value = "/sector/{sector}", method = RequestMethod.GET, produces = "application/json")
     public RestResponse getNewsBySector(@PathVariable("sector") String _sector) {
         RestResponse restResponse = new RestResponse();
@@ -92,7 +92,7 @@ public class NewsRestController {
 
         List<Company> companies = this.companySearchRepo.searchForCompany(csp);
         List<CompanyNewsItem> companyNewsItems = new ArrayList<>();
-        
+
         if (companies != null) {
             for (Company company : companies) {
                 CompanyNewsSearchProperties cnsp = new CompanyNewsSearchProperties();
@@ -109,33 +109,34 @@ public class NewsRestController {
         }
         restResponse.setObject(companyNewsItems);
         return restResponse;
-        
+
     }
-    
+
     @RequestMapping(value = "/company/{id}/{start}", method = RequestMethod.GET, produces = "application/json")
     public RestResponse getCompanyNewsForCompanyStartingWith(@PathVariable("id") String _id, @PathVariable("start") int _start) {
-        return this.getCompanyNewsForCompanyStartingWithInRange(_id, _start, 25);
+        return this.getNewsForCompanyStartingWithInRange(_id, _start, 25);
     }
-    
+
     @RequestMapping(value = "/company/{id}/{start}/{numberOfItems}", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse getCompanyNewsForCompanyStartingWithInRange(@PathVariable("id") String _id, @PathVariable("start") int _start, @PathVariable("numberOfItems") int _numberOfItems) {
-        
+    public RestResponse getNewsForCompanyStartingWithInRange(@PathVariable("id") String _id, @PathVariable("start") int _start, @PathVariable("numberOfItems") int _numberOfItems) {
+
         RestResponse restResponse = new RestResponse();
         CompanySearchProperties csp = new CompanySearchProperties();
         csp.setCompanyId(_id);
         csp.setStartResults(0);
         csp.setNumResults(_numberOfItems);
-        
+
         List<Company> companies = this.companySearchRepo.searchForCompany(csp);
         List<CompanyNewsItem> companyNewsItems = new ArrayList<>();
-        
+
         if (companies != null) {
             for (Company company : companies) {
                 CompanyNewsSearchProperties cnsp = new CompanyNewsSearchProperties();
                 cnsp.setStockExchange(company.getStockExchange());
                 cnsp.setStockSymbol(company.getStockSymbol());
                 cnsp.setSortField("recordDate");
-
+                cnsp.setStartResults(_start);
+                cnsp.setNumResults(_numberOfItems);
                 //this.companyNewsServiceImpl.submitCompanyToDownloadQueue(company);
                 List<CompanyNewsItem> cnis = this.companyNewsSearchRepo.searchForCompanyNews(cnsp);
                 if (cnis != null) {
@@ -145,15 +146,15 @@ public class NewsRestController {
         }
         restResponse.setObject(companyNewsItems);
         return restResponse;
-        
+
     }
-    
+
     @RequestMapping(value = "/company/{id}", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse getCompanyNewsForCompany(@PathVariable("id") String _id) {
+    public RestResponse getNewsForCompany(@PathVariable("id") String _id) {
         return this.getCompanyNewsForCompanyStartingWith(_id, 0);
-        
+
     }
-    
+
     @RequestMapping(value = "/symbol/{symbol}/fetch", method = RequestMethod.POST, produces = "application/json")
     public RestResponse fetchNewsForCompany(@PathVariable("symbol") String symbol) {
         RestResponse restResponse = new RestResponse();
@@ -168,6 +169,46 @@ public class NewsRestController {
         }
         //restResponse.setObject(company);
         return restResponse;
-    }    
+    }
+
+    @RequestMapping(value = "/date/{date}", method = RequestMethod.GET, produces = "application/json")
+    public RestResponse getNewsForDate(@PathVariable("date") String _date) {
+        return this.getNewsForDateStartingWith(_date, 0);
+
+    }
+
+    @RequestMapping(value = "/date/{date}/{start}", method = RequestMethod.GET, produces = "application/json")
+    public RestResponse getNewsForDateStartingWith(@PathVariable("date") String _date,@PathVariable("start") int _start) {
+        return this.getNewsForDateStartingWithInRange(_date, _start,25);
+
+    }
+    @RequestMapping(value = "/date/{date}/{start}/{numberOfItems}", method = RequestMethod.GET, produces = "application/json")
+    public RestResponse getNewsForDateStartingWithInRange(@PathVariable("date") String _date,@PathVariable("start") int _start, @PathVariable("numberOfItems") int _numberOfItems) {
+        RestResponse restResponse = new RestResponse();
+        //CompanySearchProperties csp = new CompanySearchProperties();
+        //csp.setCompanyId(_id);
+
+        //List<Company> companies = this.companySearchRepo.searchForCompany(csp);
+        List<CompanyNewsItem> companyNewsItems = new ArrayList<>();
+
+        CompanyNewsSearchProperties cnsp = new CompanyNewsSearchProperties();
+        //cnsp.setStockExchange(company.getStockExchange());
+        //cnsp.setStockSymbol(company.getStockSymbol());
+        cnsp.setSearchDate(_date);
+        cnsp.setSortField("recordDate");
+        cnsp.setSortOrder("DESC");
+        cnsp.setStartResults(_start);
+        cnsp.setNumResults(_numberOfItems);
+
+        //this.companyNewsServiceImpl.submitCompanyToDownloadQueue(company);
+        List<CompanyNewsItem> cnis = this.companyNewsSearchRepo.searchForCompanyNews(cnsp);
+        if (cnis != null) {
+            companyNewsItems.addAll(cnis);
+        }
+        restResponse.setObject(companyNewsItems);
+        return restResponse;
+    }
+
+    
     
 }
